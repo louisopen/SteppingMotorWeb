bda79bd2c30d29255c489ebedf0011cca4b52b64
Louis Huang <louisopen@gmail.com>
2019-06-16 22:38:22 +0800

    update bash

--- bootapp.sh
+++ bootapp.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+#This bash called by /home/pi/.config/autostart/autoboot.desktop
 #Git clone https://github.com/louisopen/SteppingMotorWeb
 #cd SteppingMotorWeb
-#sudo python3 ./DualStepperWeb.py
-sudo python3 /home/pi/SteppingMotorWeb/DualStepperWeb.py
-#sudo python3 /home/pi/SteppingMotorWeb/FlaskWebStepper.py
+#tr -d "\r" < oldname.sh > newname.sh   #if you can't cd to .... just do it.
+jump_dir=/home/pi/SteppingMotorWeb
+cd $jump_dir
+sudo python3 $jump_dir/DualStepperWeb.py
+#sudo python3 $jump_dir/FlaskWebStepper.py
+pwd
+exit 0
